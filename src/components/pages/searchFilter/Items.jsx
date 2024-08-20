d5c06c5b85101880c9234d0001e9c6776ae2eeb2
Happy Ruda <01036156791@nate.com>
2024-08-20 15:55:25 +0900

    key error 수정

--- src/components/pages/searchFilter/Items.jsx
+++ src/components/pages/searchFilter/Items.jsx
@@ -1,35 +1,39 @@
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import { Link } from 'react-router-dom';
+
 function Items({ items, loading }) {
-  if (loading)
+  if (loading) 
     return (
       <article aria-busy="true">
-        잠시만 기다려주세요. 상품 데이터를 불러오고 있습니다.
+        잠시만 기다려주세요. 데이터를 불러오고 있습니다.
       </article>
     );
   return (
-    <div className="row">
+    <Row>
       {items.map(item => {
         const keywords = item.keywords;
         return (
-          <article key={item.index} className="col box-shadow n42">
+          <Col key={item.id} className="box-shadow n42">
             <header>
               <div className="img"><img src={item.images} alt={item.subject} /></div>
               <div className="subject">{item.subject}</div>
             </header>
-            <div>{item.category}</div>
-            <div>{item.brand}</div>
+            <div className="category">{item.category}</div>
+            <div className="brand">{item.brand}</div>
             <div className="depart">{item.depart}</div>
             <div className="user">{item.user}</div>
             <div className="keywords">
               {keywords.map(keyword => {
                 return (
-                  <a href={keyword.link} target="_blank" key={keyword.index}>{keyword.keyword}</a>
+                  <Link key={keyword.id} target="_blank" to={keyword.link}>{keyword.keyword}</Link>
                 ); //return
               })} {/* keywords.map */}
             </div>
-          </article>
+          </Col>
         )
       })} {/* items.map */}
-    </div>
+    </Row>
   );
 }
 
